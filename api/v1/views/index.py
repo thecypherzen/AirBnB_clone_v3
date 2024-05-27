84415cd36a197e7a66667d6cb31115169c95efe2
William Inyam <sw.inyam@outlook.com>
2024-05-27 23:57:25 +0000

    fix(hbnb3:api): attempt bug fixes

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,10 +2,24 @@
 """ Defines interface for index"""
 
 from api.v1.views import app_views
-import json
+from flask import jsonify
+import models
+from models import storage
 
-
-@app_views.route('/status')
+@app_views.route('/status', strict_slashes=False)
 def get_status():
     """checks status of server"""
-    return json.loads('{"status": "OK"}')
+    return jsonify('{"status": "OK"}')
+
+@app_views.route('/stats', strict_slashes=False)
+def get_models_stats():
+    """retrieves the number of each objects by type"""
+    res = f"""
+          'amenities': '{storage.count(models.amenity.Amenity)}',
+          'cities'   : '{storage.count(models.city.City)}',
+          'places'   : '{storage.count(models.place.Place)}',
+          'reviews'  : '{storage.count(models.review.Review)}',
+          'states'   : '{storage.count(models.state.State)}',
+          'users'    : '{storage.count(models.user.User)}'
+          """
+    return jsonify(res)
