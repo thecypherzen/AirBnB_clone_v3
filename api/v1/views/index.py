34af682b88edb7adde64c0a99742fbe04a2b7cb0
William Inyam <sw.inyam@outlook.com>
2024-05-27 22:44:44 +0000

    build(hbnbv3:api): build api task 3 codebase

---
+++ api/v1/views/index.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+from api.v1.views import app_views
+import json
+
+
+@app_views.route('/status')
+def get_status():
+    """checks status of server"""
+    return json.loads('{"status": "OK"}')
