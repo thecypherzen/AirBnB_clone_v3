1298d9f83b75861fa63e42dea853087d0b576664
Valentine Nyibiam <saanmoyolnyibiam@gmail.com>
2024-05-28 09:15:54 +0000

    build(hbnb3:api1): add amenity view

--- test_get_count.py
+++ test_get_count.py
@@ -2,39 +2,32 @@
 """ Test .get() and .count() methods
 """
 from models import storage
-from models.state import State
-from models.city import City
+from models.amenity import Amenity
 
 
-storage.new(City(state_id="50ce5adb-ef1c-494e-ae37-4b95d67d3060",
-                 name="Garki"))
-storage.new(City(state_id="50ce5adb-ef1c-494e-ae37-4b95d67d3060",
-                 name="Guzape"))
-storage.new(City(state_id="50ce5adb-ef1c-494e-ae37-4b95d67d3060",
-                 name="Apo"))
-storage.new(City(state_id="50ce5adb-ef1c-494e-ae37-4b95d67d3060",
-                 name="Maitama"))
-storage.new(City(state_id="50ce5adb-ef1c-494e-ae37-4b95d67d3060",
-                 name="CBD"))
-storage.new(City(state_id="164da99f-5463-414b-94cb-463b7b5c69ae",
-                 name="Anyigba"))
-storage.new(City(state_id="164da99f-5463-414b-94cb-463b7b5c69ae",
-                 name="Lokoja"))
-storage.new(City(state_id="164da99f-5463-414b-94cb-463b7b5c69ae",
-                 name="Kabba"))
-storage.new(City(state_id="a8bd605a-43f1-45fe-8d7c-84f5ffb2f86b",
-                 name="Auskachi"))
-storage.new(City(state_id="a8bd605a-43f1-45fe-8d7c-84f5ffb2f86b",
-                 name="Dabasama"))
 
-storage.new(City(state_id="2206f4f1-ed50-4e5f-86fa-2c9490876143",
-                 name="Awka"))
-storage.new(City(state_id="a8bd605a-43f1-45fe-8d7c-84f5ffb2f86b",
-                 name="Ariaria"))
-storage.new(City(state_id="2206f4f1-ed50-4e5f-86fa-2c9490876143",
-                 name="Idemili"))
+storage.new(Amenity(name="Internet"))
+storage.new(Amenity(name="Swimming pool"))
+storage.new(Amenity(name="Kitchen"))
+storage.new(Amenity(name="Free Wifi"))
+storage.new(Amenity(name="Car Rental"))
+storage.new(Amenity(name="Breakfast"))
+storage.new(Amenity(name="Fast food"))
+storage.new(Amenity(name="Lunch"))
+storage.new(Amenity(name="Dinner"))
+storage.new(Amenity(name="Laundry"))
+storage.new(Amenity(name="Chaffeur"))
+storage.new(Amenity(name="Room service"))
+storage.new(Amenity(name="Security"))
+storage.new(Amenity(name="CCTV"))
+storage.new(Amenity(name="Free ride"))
+storage.new(Amenity(name="TV"))
+storage.new(Amenity(name="Gym"))
+storage.new(Amenity(name="Microwave"))
+storage.new(Amenity(name="Launge"))
+storage.new(Amenity(name="Wine bar"))
+storage.new(Amenity(name="Bathroom"))
+storage.new(Amenity(name="Warm water"))
 
-storage.new(City(state_id="fb258820-5394-4872-9d57-8ecb8ced9ed8",
-                 name="Kafanchan"))
 storage.save()
 storage.close()
