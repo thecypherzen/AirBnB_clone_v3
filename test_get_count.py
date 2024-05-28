8335fb419ca79095e67aee3b3c957a5d36fdee5a
Valentine Nyibiam <saanmoyolnyibiam@gmail.com>
2024-05-28 08:11:30 +0000

    build(hbnb3:views): add cities view blueprint

--- test_get_count.py
+++ test_get_count.py
@@ -3,9 +3,38 @@
 """
 from models import storage
 from models.state import State
+from models.city import City
 
-print("All objects: {}".format(storage.count()))
-print("State objects: {}".format(storage.count(State)))
 
-first_state_id = list(storage.all(State).values())[0].id
-print("First state: {}".format(storage.get(State, first_state_id)))
+storage.new(City(state_id="50ce5adb-ef1c-494e-ae37-4b95d67d3060",
+                 name="Garki"))
+storage.new(City(state_id="50ce5adb-ef1c-494e-ae37-4b95d67d3060",
+                 name="Guzape"))
+storage.new(City(state_id="50ce5adb-ef1c-494e-ae37-4b95d67d3060",
+                 name="Apo"))
+storage.new(City(state_id="50ce5adb-ef1c-494e-ae37-4b95d67d3060",
+                 name="Maitama"))
+storage.new(City(state_id="50ce5adb-ef1c-494e-ae37-4b95d67d3060",
+                 name="CBD"))
+storage.new(City(state_id="164da99f-5463-414b-94cb-463b7b5c69ae",
+                 name="Anyigba"))
+storage.new(City(state_id="164da99f-5463-414b-94cb-463b7b5c69ae",
+                 name="Lokoja"))
+storage.new(City(state_id="164da99f-5463-414b-94cb-463b7b5c69ae",
+                 name="Kabba"))
+storage.new(City(state_id="a8bd605a-43f1-45fe-8d7c-84f5ffb2f86b",
+                 name="Auskachi"))
+storage.new(City(state_id="a8bd605a-43f1-45fe-8d7c-84f5ffb2f86b",
+                 name="Dabasama"))
+
+storage.new(City(state_id="2206f4f1-ed50-4e5f-86fa-2c9490876143",
+                 name="Awka"))
+storage.new(City(state_id="a8bd605a-43f1-45fe-8d7c-84f5ffb2f86b",
+                 name="Ariaria"))
+storage.new(City(state_id="2206f4f1-ed50-4e5f-86fa-2c9490876143",
+                 name="Idemili"))
+
+storage.new(City(state_id="fb258820-5394-4872-9d57-8ecb8ced9ed8",
+                 name="Kafanchan"))
+storage.save()
+storage.close()
