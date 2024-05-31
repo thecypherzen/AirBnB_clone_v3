d536e5f37f6a1ffa6c1872b24e2341321da264ae
William Inyam <sw.inyam@outlook.com>
2024-05-31 12:45:55 +0000

    build(hbnb3:views): add reviews view blueprint

--- get_count_test.py
+++ get_count_test.py
@@ -7,7 +7,7 @@ from models.amenity import Amenity
 from models.state import State
 from models.city import City
 from models.place import Place
-
+from models.review import Review
 
 ''' Users
 storage.new(User(first_name="John", last_name="Kaase",
@@ -36,10 +36,11 @@ storage.new(User(email="john.k@testdb.com",
 
 '''Amenities
 
-storage.new(Amenity(name="Television"))
-storage.new(Amenity(name="WIFI"))
-storage.new(Amenity(name="Car Hire"))
-storage.new(Amenity(name="Swimming pool"))
+storage.new(Amenity(name="Air condition"))
+storage.new(Amenity(name="Natural light"))
+storage.new(Amenity(name="Sound system"))
+storage.new(Amenity(name="Treadmill"))
+storage.new(Amenity(name="Standby gen"))
 '''
 
 '''States
@@ -144,5 +145,61 @@ storage.new(Place(
 ))
 '''
 
+'''Reviews
+#storage.new(Review(
+#    place_id="e8de27f0-8113-4e8d-8f80-e819940ed9a9",
+#    user_id="78c19319-68d2-4465-a5ab-dfa86dc19689",
+#    text="Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts."
+#))
+
+
+storage.new(Review(
+    place_id="f48f85b6-1734-44de-a628-24982f7fec63",
+    user_id="93985b3a-b862-45eb-959b-6e656fe125d9",
+    text="Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean."
+))
+storage.new(Review(
+    place_id="b529c49a-7cc8-4694-b4f6-b08824f70177",
+    user_id="4a913e6a-482c-45cb-87e5-95dae9c5af5b",
+    text="A small river named Duden flows by their place and supplies"
+))
+storage.new(Review(
+    place_id="9680245a-498a-4e8f-8e50-de7f9ebb7cde",
+    user_id="027867a6-569b-4aab-bd19-725fbc1a19c6",
+    text="Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts."
+))
+storage.new(Review(
+    place_id="6ebd933f-bef7-4512-aba4-796850090386",
+    user_id="2b3da38f-0a11-41dc-8420-8f1b9f89f48e",
+    text="but the Little Blind Text didn’t listen. She packed her seven versalia, put her initial into the belt and made herself on the way. When she reached the first hills of the Italic Mountains, she had a last view back on the skyline of her hometown"
+))
+storage.new(Review(
+    place_id="6b043c39-13c3-4493-a35b-84db0616f9e6",
+    user_id="027867a6-569b-4aab-bd19-725fbc1a19c6",
+    text="The Big Oxmox advised her not to do so, because there were thousands of bad Commas, wild Question Marks "
+))
+storage.new(Review(
+    place_id="6aa2b564-8e59-4ac2-b8a1-2eff0a35644e",
+    user_id="2b3da38f-0a11-41dc-8420-8f1b9f89f48e",
+    text="Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic life One day however a small line of blind text by the name of Lorem Ipsum decided to leave for the far World of Grammar. "
+))
+storage.new(Review(
+    place_id="5903a8ac-f64a-4bf3-921d-bce9b65bb4bb",
+    user_id="027867a6-569b-4aab-bd19-725fbc1a19c6",
+    text="It is a paradisematic country, in which roasted parts of sentences fly into your mouth."
+))
+storage.new(Review(
+    place_id="559d132d-6c58-4778-8a9d-1dc8db3473c1",
+    user_id="93985b3a-b862-45eb-959b-6e656fe125d9",
+    text="Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts."
+))
+storage.new(Review(
+    place_id="506cffa8-cf14-4bbd-b815-8d50242a8a1a",
+    user_id="4a913e6a-482c-45cb-87e5-95dae9c5af5b",
+    text="Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts."
+))
+'''
+'''
 storage.save()
 storage.close()
+'''
